b844b83575934c292db955a5c3e2cb3ee5366a35
Steffen Svenningsen <stef7625@gmail.com>
2023-06-29 12:16:57 +0200

    Create basic layout structure for list item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,19 @@
+import Pen from "../images/pen.png";
+
 export default function TodoItem({
     todo,
     onEditClick,
     onDeleteClick
   }) {
     return (
-      <li key={todo.id}>
+      <li className="todo-item" key={todo.id}>
         {todo.text}
-        <button onClick={() => onEditClick(todo)}>Ændre</button>
-        <button onClick={() => onDeleteClick(todo.id)}>Færdig</button>
+        <div className="btn-container">
+          <button className="change-btn" onClick={() => onEditClick(todo)}>
+            <img src={Pen} width={18} height={18} alt="Ændre opgaven" />
+          </button>
+          <button onClick={() => onDeleteClick(todo.id)}>X</button>
+        </div>
       </li>
     );
   }
