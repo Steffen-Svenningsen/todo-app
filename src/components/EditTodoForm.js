532d30e1ab23aaecf3d38cd6cf73ec6f4a0611ee
Steffen Svenningsen <stef7625@gmail.com>
2023-07-04 11:33:02 +0200

    Create more user-friendly editing layout

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -5,15 +5,17 @@ export default function EditForm({
     onEditFormSubmit
   }) {
     return (
-      <form className="btn-form" onSubmit={onEditFormSubmit}>
-        <input
-          name="updateTodo"
-          type="text"
-          placeholder="Update todo"
-          value={currentTodo.text}
-          onChange={onEditInputChange}
-        />
-        <button className="save-btn" type="submit" onClick={onEditFormSubmit}>Gem</button>
-      </form>
+      <div className="edit-popup">
+        <form className="edit-form" onSubmit={onEditFormSubmit}>
+          <input
+            name="updateTodo"
+            type="text"
+            placeholder="Update todo"
+            value={currentTodo.text}
+            onChange={onEditInputChange}
+          />
+          <button className="save-btn" type="submit" onClick={onEditFormSubmit}>Gem</button>
+        </form>
+      </div>
     );
   }
